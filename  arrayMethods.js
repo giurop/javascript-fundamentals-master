a8bf546d109ec9ba25689e62a005a68829510bfc
Giu Roperto <giu.roperto@gmail.com>
2019-12-14 14:01:56 -0300

    7 out of 8 arrayMethods

---  arrayMethods.js
+++  arrayMethods.js
@@ -14,13 +14,11 @@ const checkSum = (arr) => arr.reduce((a, b) => a + b) % 2 === 0 ? 'even' : 'odd'
 
 // ######################################### //
 
-//! Given an array of numbers, write a function evenNumbers that returns a new array only with the even numbers of the original array.
+//* Given an array of numbers, write a function evenNumbers that returns a new array only with the even numbers of the original array.
 
 const arrEx8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenNumbers = (arr) => {
-  // code here
-};
+const evenNumbers = (arr) => arr.filter((num) => num % 2 === 0);
 
 // ######################################### //
 
@@ -46,15 +44,14 @@ const arrayBy2 = (arr) => console.log(arr.map((num) => multiplyBy2(num)));
 
 // ######################################### //
 
-//! Create a function that takes a number and returns true if the number is odd and false if the number is even
+//* Create a function that takes a number and returns true if the number is odd and false if the number is even
 
-// code here
-
-// ######################################### //
-
-//! Using any array method and the function built in the previous question, make a loop that return only the odd numbers.
-
-// code here
+const isOdd = (num) => num % 2 !== 0;
 
 // ######################################### //
 
+//* Using any array method and the function built in the previous question, make a loop that return only the odd numbers.
+
+const arrOdd = (array) => array.filter((num) => isOdd(num));
+
+// ######################################### //
