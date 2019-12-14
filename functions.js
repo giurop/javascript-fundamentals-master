2ac7d0aae8d89a900fb4fba085076cff591534e2
Giu Roperto <giu.roperto@gmail.com>
2019-12-14 20:38:21 -0300

    2 out of 18 functions

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 //*********************************** EXAMPLE ***********************************//
-//! adds 2 to inputted number
+//* adds 2 to inputted number
 // ex: addTwo(5); -> 7
 // ex: addTwo(6); -> 8
 //*** ES5 ***//
@@ -8,216 +8,220 @@ function addTwo(number) {
     return result;
 }
 // OR
-function addTwo(number) {
+function addTwo1(number) {
     return number + 2;
     
 }
 //*** ES6 ***//
-const addTwo = number => {
+const addTwo2 = number => {
     const result = number + 2;
     return result;
 }
 // OR
-const addTwo = number => number + 2;
+const addTwo3 = number => number + 2;
 
 //*********************************** PRACTICE ***********************************//
 
-//! adds the two inputted numbers together
+//* adds the two inputted numbers together
 // ex: add(5, 10); -> 15
 //*** ES5 ***//
-function add(num1, num2) {
-
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 //*** ES6 ***//
-const add = (num1,num2) => // write your code here
+const add = (num1,num2) => num1 + num2;
+
+// console.log(add(5, 10));
 
 //--------------------------------------------------------------------------------//
-//! adds all three inputted numbers together
+//* adds all three inputted numbers together
 // ex: addd(1,2,3); -> 6
 // ex: addd(2,4,2); -> 8
 //*** ES5 ***//
-function addd(value1, value2, value3) {
-
-}
+// function addd(value1, value2, value3) {
+//     return value1 + value2 + value3;
+// }
 //*** ES6 ***//
-const addd = (value1, value2, value3) => // write your code here
+const addd = (value1, value2, value3) => value1 + value2 + value3;
 
-//--------------------------------------------------------------------------------//
-//! subtracts the second inputted number from the first inputted number
-// ex: subtract(5, 10); -> -5
-//*** ES5 ***//
-function subtract(n1, n2) {
+// console.log(addd(2,3,5));
 
-}
-//*** ES6 ***//
-const subtract = (n1,n2) =>  // write your code here
+// //--------------------------------------------------------------------------------//
+// //! subtracts the second inputted number from the first inputted number
+// // ex: subtract(5, 10); -> -5
+// //*** ES5 ***//
+// function subtract(n1, n2) {
 
-//--------------------------------------------------------------------------------//
-//! multiplies the inputted number by 2
-// ex: multiplyByTwo(5); -> 10
-// ex: multiplyByTwo(6); -> 12
-//*** ES5 ***//
-function multiplyByTwo(numero) {
+// }
+// //*** ES6 ***//
+// const subtract = (n1,n2) =>  // write your code here
 
-}
-//*** ES6 ***//
-const multiplyByTwo = numero => // write your code here
+// //--------------------------------------------------------------------------------//
+// //! multiplies the inputted number by 2
+// // ex: multiplyByTwo(5); -> 10
+// // ex: multiplyByTwo(6); -> 12
+// //*** ES5 ***//
+// function multiplyByTwo(numero) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! multplies the two inputted numbers together
-// ex: multiply(1,2); -> 2
-// ex: multiply(2,2); -> 4
-function multiply(val1, val2) {
+// }
+// //*** ES6 ***//
+// const multiplyByTwo = numero => // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! multplies the two inputted numbers together
+// // ex: multiply(1,2); -> 2
+// // ex: multiply(2,2); -> 4
+// function multiply(val1, val2) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! multiplies all three inputted numbers together
-// ex: multiplyy(1,2,3); -> 6
-// ex: multiplyy(2,2,3); -> 12
-function multiplyy(a, b, c) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! multiplies all three inputted numbers together
+// // ex: multiplyy(1,2,3); -> 6
+// // ex: multiplyy(2,2,3); -> 12
+// function multiplyy(a, b, c) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! returns the square of the inputted number
-// ex: square(5); -> 25
-function square(value) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! returns the square of the inputted number
+// // ex: square(5); -> 25
+// function square(value) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! determines if the number is odd
-//! outputs true if the number is odd
-//! outputs false if the number is even
-// ex: isOdd(4); -> true
-// ex: isOdd(5); -> false
-function isOdd(num) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! determines if the number is odd
+// //! outputs true if the number is odd
+// //! outputs false if the number is even
+// // ex: isOdd(4); -> true
+// // ex: isOdd(5); -> false
+// function isOdd(num) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! determines if the number is negative
-//! outputs true if the number is negative
-//! outputs false if the number is zero or positive
-// ex: isNegative(-1); -> true
-// ex: isNegative(1); -> false
-// ex: isNegative(0); -> false
-function isNegative(num) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! determines if the number is negative
+// //! outputs true if the number is negative
+// //! outputs false if the number is zero or positive
+// // ex: isNegative(-1); -> true
+// // ex: isNegative(1); -> false
+// // ex: isNegative(0); -> false
+// function isNegative(num) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! returns the absolute value of the inputted number
-// ex: positive(-1); -> 1
-// ex: positive(1); -> 1
-function positive(num) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! returns the absolute value of the inputted number
+// // ex: positive(-1); -> 1
+// // ex: positive(1); -> 1
+// function positive(num) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! Returns boolean of whether argument is classified as a Number object
-// isNumber(5); → true
-// isNumber('hi'); → false
-function isNumber(value) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! Returns boolean of whether argument is classified as a Number object
+// // isNumber(5); → true
+// // isNumber('hi'); → false
+// function isNumber(value) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! Returns boolean of whether argument is classified as an Array object
-// isArray(5); → false
-// isArray([1,2,3]); → true
-function isArray(value) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! Returns boolean of whether argument is classified as an Array object
+// // isArray(5); → false
+// // isArray([1,2,3]); → true
+// function isArray(value) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! Returns boolean of whether argument is classified as an Object
-// isObject(5); → false
-// isObject([1,2,3]); → false
-function isObject(value) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! Returns boolean of whether argument is classified as an Object
+// // isObject(5); → false
+// // isObject([1,2,3]); → false
+// function isObject(value) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! return boolean of whether argument is classified as null
-// isNull(null); -> true
-// isNull(5); -> false
-function isNull(value) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! return boolean of whether argument is classified as null
+// // isNull(null); -> true
+// // isNull(5); -> false
+// function isNull(value) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! this function accepts an array of numbers
-//! and returns an array of only the odd numbers
-// ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
-function returnOdds(array) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! this function accepts an array of numbers
+// //! and returns an array of only the odd numbers
+// // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
+// function returnOdds(array) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
+// }
+// //*** ES6 ***//
+// // write your code here
 
-//! this function accepts an array of numbers
-//! and returns an array of only the even numbers
-// ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
-function returnEvens(array) {
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
 
-}
-//*** ES6 ***//
-// write your code here
+// //! this function accepts an array of numbers
+// //! and returns an array of only the even numbers
+// // ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
+// function returnEvens(array) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! returns only the max element from the inputted array of numbers
-// ex: findMax([1,25,6,3]); -> 25
-function findMax(array) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! returns only the max element from the inputted array of numbers
+// // ex: findMax([1,25,6,3]); -> 25
+// function findMax(array) {
 
-//--------------------------------------------------------------------------------//
-//*** ES5 ***//
-//! remove leading and trailing whitespace or specified characters from string
-// trim(' hello '); -> 'hello'
-function trim(string) {
+// }
+// //*** ES6 ***//
+// // write your code here
 
-}
-//*** ES6 ***//
-// write your code here
+// //--------------------------------------------------------------------------------//
+// //*** ES5 ***//
+// //! remove leading and trailing whitespace or specified characters from string
+// // trim(' hello '); -> 'hello'
+// function trim(string) {
 
-//--------------------------------------------------------------------------------//
+// }
+// //*** ES6 ***//
+// // write your code here
+
+// //--------------------------------------------------------------------------------//
